2c9bc75e3feced7dffcb745a386cf5141e6f27ea
Kit Langton <kitlangton@gmail.com>
2016-03-10 16:58:42 -0500

    grid

--- snake.js
+++ snake.js
@@ -1,39 +1,54 @@
-$(document).ready(function(){
-  var speed = 50;
-  var head = $('.head');
-  var body = $('.body');
-  var tail = $('.tail');
-  head.css({
-    left: 280,
-    top: 280
-  })
-  var headPos = head.position();
-  body.css({
-    left: headPos.left -50,
-    top: headPos.top,
-  })
-  var bodyPos = body.position();
-  tail.css({
-    left: bodyPos.left -50,
-    top: bodyPos.top,
-  })
+var model = {
+  size: 30,
 
-  var moveBody = function() {
-    head.css({
-      left: head.position().left + speed,
-      top: headPos.top
-    })
-    body.css({
-      left: body.position().left + speed,
-      top: headPos.top
-    })
-    tail.css({
-      left: tail.position().left + speed,
-      top: headPos.top
-    })
+  grid: { '4,4': 4 },
+
+  init: function() {
+
+  },
+
+  gridGet: function(x, y) {
+    return this.grid[x.toString() + ',' + y.toString()];
+  },
+
+  bodyPart: function BodyPart(orientation, x, y, type, next) {
+    this.orientation = orientation;
+    this.x = x;
+    this.y = y;
+    this.type = type;
+    this.next = next
   }
 
-  setInterval(function(){
-    moveBody()
-  }, 500)
+};
+
+var controller = {
+  gridSize: model.size,
+
+  init: function() {
+    view.init()
+  },
+};
+
+var view = {
+
+  init: function() {
+    this.render();
+  },
+
+  render: function() {
+    for (var x = 0; x < controller.gridSize; ++x) {
+      var row = $("<div class='row'></div>");
+      for (var y = 0; y < controller.gridSize; ++y) {
+        var cell = $("<div class='cell'></div>");
+        cell.data('x', x.toString());
+        cell.data('y', y.toString());
+        row.append(cell);
+      }
+      $('#grid').append(row);
+    }
+  }
+};
+
+$(document).ready(function(){
+  controller.init();
 });
