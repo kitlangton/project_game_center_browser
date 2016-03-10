f9c3f295fa3f32f915e5e09e67ca1f7c97b845e9
Kit Langton <kitlangton@gmail.com>
2016-03-10 17:39:59 -0500

    whee

--- snake.js
+++ snake.js
@@ -2,30 +2,40 @@
 
 var model = {
   size: 15,
-
-  grid: { '4,4': 4 },
+  grid: { },
 
   init: function() {
     // need to get initial location here
 
-    this.makeSnake( 5, 7);
+    this.snake = new this.Snake( 5, 7);
+    this.addSnakeToGrid();
   },
 
-  makeSnake: function( startX, startY ) {
-    this.head: new BodyPart( 'right', startX, startY, 'head', this.body   ),
-    this.body: new BodyPart( 'right', startX - 1, startY,'body', this.tail   ),
-    this.tail: new BodyPart( 'up', startX - 1, startY - 1, 'tail', null )
+  Snake: function ( startX, startY ) {
+    this.tail = new model.BodyPart( 'up', startX - 1, startY - 1, 'tail', null );
+    this.body = new model.BodyPart( 'right', startX - 1, startY,'body', this.tail   );
+    this.head = new model.BodyPart( 'right', startX, startY, 'head', this.body  );
   },
 
-  // getOrientation: function( grid1, grid2) {
+  addSnakeToGrid: function() {
+    this.addItemToGrid(this.snake.head);
+    this.addItemToGrid(this.snake.body);
+    this.addItemToGrid(this.snake.tail);
+  },
 
-  // },
+  addItemToGrid: function(item) {
+    this.addToGrid(item.x, item.y, item)
+  },
 
   gridGet: function(x, y) {
     return this.grid[x.toString() + ',' + y.toString()];
   },
 
-  bodyPart: function BodyPart(orientation, x, y, type, next) {
+  addToGrid: function(x, y, value) {
+    this.grid[x.toString() + ',' + y.toString()] = value;
+  },
+
+  BodyPart: function (orientation, x, y, type, next) {
     this.orientation = orientation;
     this.x = x;
     this.y = y;
@@ -37,6 +47,10 @@ var model = {
 
 var controller = {
   gridSize: model.size,
+
+  objectAt: function(x,y) {
+    return model.gridGet(x,y);
+  },
 
   init: function() {
     model.init();
@@ -51,10 +65,18 @@ var view = {
   },
 
   render: function() {
-    for (var x = 0; x < controller.gridSize; ++x) {
+    $('#grid').html("");
+    for (var y = controller.gridSize - 1; y >= 0; --y) {
       var row = $("<div class='row'></div>");
-      for (var y = 0; y < controller.gridSize; ++y) {
+      for (var x = 0; x < controller.gridSize; ++x) {
         var cell = $("<div class='cell'></div>");
+        var object = controller.objectAt(x,y);
+
+        if (object) {
+          cell.addClass(object.type);
+          cell.addClass(object.orientation);
+        }
+
         cell.data('x', x.toString());
         cell.data('y', y.toString());
         row.append(cell);
