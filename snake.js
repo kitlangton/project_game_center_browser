8e6a7282ff95daca335bacdb90e3b0826c2e41e8
Kit Langton <kitlangton@gmail.com>
2016-03-10 20:01:07 -0500

    MEOW

--- snake.js
+++ snake.js
@@ -3,12 +3,30 @@ var model = {
   gameState: 'running',
   grid: { },
   direction: 'right',
+  audioFiles: ['meow.mp3','hello.m4a','meow4.mp3','meow5.mp3','meow6.mp3'],
+  catSounds: [],
 
   init: function() {
     // need to get initial location here
-
     this.snake = new this.Snake( 5, 7);
     this.addSnakeToGrid();
+    for (var i = 0; i < 4; ++i) {
+      this.catSounds.push(new Audio(this.audioFiles[i]));
+    }
+  },
+
+  setDirection: function(direction) {
+    if ( (direction == 'up' && this.direction == 'down') || (direction == 'down' && this.direction == 'up') ||
+        (direction == 'left' && this.direction == 'right') || (direction == 'right' && this.direction == 'left')) {
+      return;
+    }
+    this.direction = direction;
+  },
+
+  playMeow: function() {
+    var random = Math.floor(Math.random() * this.catSounds.length);
+    var sound = this.catSounds[random];
+    sound.play();
   },
 
   Snake: function ( startX, startY ) {
@@ -21,10 +39,70 @@ var model = {
   addSnakeToGrid: function() {
     var bodypart = this.snake.head;
     while( bodypart ) {
-      this.addItemToGrid( bodypart )
-      bodypart = bodypart.next;
+      this.addItemToGrid( bodypart );
+        bodypart = bodypart.next;
     }
   },
+
+  spawnFood: function() {
+    var random = Math.floor(Math.random() * 13);
+    if (random == 0) {
+      var coord = this.randomGridPoint();
+      this.addItemToGrid(new this.Food(coord.x, coord.y));
+    }
+  },
+
+  randomGridPoint: function() {
+    do {
+      var randX = Math.floor(Math.random() * 15);
+      var randY = Math.floor(Math.random() * 15);
+    } while (this.gridGet(randX, randY))
+    return { x: randX, y: randY};
+  },
+
+  Food: function(x, y) {
+    this.x = x;
+    this.y = y;
+    this.type = 'baby';
+    this.orientation = 'right';
+  },
+
+  nextLocation: function() {
+    var coord = {}
+    switch(this.direction){
+      case 'up':
+        coord.x = this.snake.head.x;
+        coord.y = this.snake.head.y + 1;
+        break;
+      case 'down':
+        coord.x = this.snake.head.x;
+        coord.y = this.snake.head.y - 1;
+        break;
+      case 'left':
+        coord.x = this.snake.head.x - 1;
+        coord.y = this.snake.head.y;
+        break;
+      case 'right':
+        coord.x = this.snake.head.x + 1;
+        coord.y = this.snake.head.y;
+        break;
+    }
+    return coord;
+  },
+
+  checkCollision: function() {
+    var coord = this.nextLocation();
+    var object = this.gridGet(coord.x, coord.y);
+    if (object) {
+      if (object.type == 'baby') {
+        this.playMeow();
+        this.extendBody();
+      } else if (object.type == 'body') {
+        this.gameState = 'finished';
+      }
+    }
+  },
+
 
   extendBody: function() {
     var tail = this.snake.tail;
@@ -36,7 +114,6 @@ var model = {
   },
 
   removeSnakeFromGrid: function() {
-    this.grid = {}
     var bodypart = this.snake.head;
     while( bodypart ) {
       this.revmoveItemFromGrid( bodypart );
@@ -93,18 +170,18 @@ var model = {
     for (var i = this.snake.bodyParts.length - 1; i > 0; --i) {
       var bodyPart = this.snake.bodyParts[i];
       bodyPart.order = i
-      var nextBodyPart = this.snake.bodyParts[i-1]
-      bodyPart.x = nextBodyPart.x;
-      bodyPart.y = nextBodyPart.y;
+        var nextBodyPart = this.snake.bodyParts[i-1]
+        bodyPart.x = nextBodyPart.x;
+        bodyPart.y = nextBodyPart.y;
 
-      var positionString = bodyPart.x.toString() + ',' + bodyPart.y.toString();
-      if( snakeGridPoints.indexOf(positionString) !== -1 ) {
-        console.log('im here');
-        this.gameState = 'finished';
-      }
+        var positionString = bodyPart.x.toString() + ',' + bodyPart.y.toString();
+        if( snakeGridPoints.indexOf(positionString) !== -1 ) {
+          console.log('im here');
+          this.gameState = 'finished';
+        }
 
-      snakeGridPoints.push( positionString );
-      bodyPart.orientation = nextBodyPart.orientation;
+        snakeGridPoints.push( positionString );
+        bodyPart.orientation = nextBodyPart.orientation;
     }
 
     // this.moveSnakeHead();
@@ -155,10 +232,7 @@ var controller = {
   gridSize: model.size,
 
   setDirection: function(direction) {
-    if (direction == 'down') {
-      model.extendBody();
-    }
-    model.direction = direction;
+    model.setDirection(direction);
     view.render();
   },
 
@@ -171,7 +245,9 @@ var controller = {
     view.init();
     setInterval(function() {
       if( model.gameState !== 'finished' ) {
+        model.checkCollision();
         model.moveSnake();
+        model.spawnFood();
         view.render();
       }
     },150);
@@ -195,7 +271,7 @@ var view = {
         if (object) {
           cell.addClass(object.type);
           cell.addClass(object.orientation);
-          cell.css('zIndex', object.order);
+          // cell.css('zIndex', object.order);
         }
 
         cell.data('x', x.toString());
